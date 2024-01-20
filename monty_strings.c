cf1eb75dac981f84d7bdf02dcdc93e17f35ad5d1
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-20 13:37:02 +0100

    SAFE PCHAR

--- monty_strings.c
+++ monty_strings.c
@@ -62,7 +62,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (glob->size < 1)
 	{
-		fprintf(stderr, "L%u: can't pchar, stack is empty\n", line_number);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		free(glob->buffer);
 		free_glob(glob);
 		exit(EXIT_FAILURE);
